b07884af536abb7ef2ce38cb6ed54c60100cb7e2
SiyuanWangw <2541015865@qq.com>
2025-02-20 16:26:28 -0800

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,6 @@
 nvidia-smi
 
-python informative_search.py \
+python Src/informative_search.py \
     --base_model '../Llama3.2-3B-Instruct' \
     --run_log '3B_grounding_0.7_novelty_0.4_self' \
     --premise_info_thres 0.7 \
